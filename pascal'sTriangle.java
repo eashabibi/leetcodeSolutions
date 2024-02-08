4464ef92bb7cae12586e7446ca1a22012162983c
Epanagandla Asha Bibi <e.ashabibi123@gmail.com>
2024-02-08 16:29:53 +0530

    Create pascal'sTriangle.java

---
+++ pascal'sTriangle.java
@@ -0,0 +1,30 @@
+// METHOD:  combinations
+---------------------------
+class Solution {
+    
+    public List<List<Integer>> generate(int numRows) {
+         List<List<Integer>> result=new ArrayList<>();
+         if (numRows==0){
+             return result;
+         }
+        List<Integer> firstRow=new ArrayList<>();
+        firstRow.add(1);
+        result.add(firstRow);
+        if(numRows==1) 
+          return result;
+        for(int i=1;i<numRows;i++){
+        List<Integer> prevRow=result.get(i-1);
+
+        List<Integer> newRow=new ArrayList<>();
+        newRow.add(1);
+        for (int j = 0; j < i - 1; j++) {
+        newRow.add(prevRow.get(j) + prevRow.get(j + 1));
+      }
+      newRow.add(1);
+      result.add(newRow);
+        }
+
+        return result;
+    }
+
+    }
